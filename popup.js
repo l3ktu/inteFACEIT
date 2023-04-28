6e0a8d71efffbe7838646cc7afb69a41cfe9b7ec
l3ktu <l3ktuu@gmail.com>
2023-04-28 18:15:23 +0300

    main

--- popup.js
+++ popup.js
@@ -1,15 +1,52 @@
+const options = {
+  enabled: true,
+  hideNotJoinable: true,
+  hideNotReady: true,
+  hidePlaying: true,
+  hideQueuing: true,
+  outputDebuggingMessages: false,
+};
+
 document.addEventListener('DOMContentLoaded', function() {
-    // Add an event listener to the menu items to update the selected class
-    const menuItems = document.querySelectorAll('.menu a');
-    menuItems.forEach((item) => {
-      item.addEventListener('click', (event) => {
-        // Remove the selected class from all menu items
-        menuItems.forEach((item) => {
-          item.classList.remove('selected');
-        });
-        // Add the selected class to the clicked menu item
-        event.target.classList.add('selected');
+  // Add an event listener to the menu items to update the selected class and show the corresponding content
+  const menuItems = document.querySelectorAll('.menu a');
+  const categoryContents = document.querySelectorAll('.category-content');
+  menuItems.forEach((item, index) => {
+    item.addEventListener('click', (event) => {
+      // Remove the selected class from all menu items
+      menuItems.forEach((item) => {
+        item.classList.remove('selected');
       });
+      // Add the selected class to the clicked menu item
+      event.target.classList.add('selected');
+      // Hide all category contents
+      categoryContents.forEach((content) => {
+        content.style.display = 'none';
+      });
+      // Show the corresponding category content
+      categoryContents[index].style.display = 'block';
     });
   });
-  
+  for (const key in options) {
+    document.getElementById(key).addEventListener('change', (event) => {
+       options[key] = event.target.checked;
+       chrome.storage.sync.set({options}, () => {
+          // Update status to let user know changes were saved.
+          const status = document.getElementById('status');
+          status.textContent = 'Changes saved.';
+          setTimeout(() => {
+             status.textContent = '';
+          }, 750);
+       });
+    });
+ }
+
+   // if enabled state is changes in the application, automatically set/remove checkmark for 'enabled'.
+   chrome.storage.onChanged.addListener((changes, area) => {
+    if (area === 'sync' && changes.options?.newValue) {
+       changes = changes.options.newValue;
+       options.enabled = changes.enabled;
+       document.getElementById('enabled').checked = options.enabled;
+    }
+ })
+});
