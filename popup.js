c417d1f31d139f2faa607f2a33ee316ab928d3d8
l3ktu <l3ktuu@gmail.com>
2023-04-28 18:28:25 +0300

    main

--- popup.js
+++ popup.js
@@ -8,6 +8,33 @@ const options = {
 };
 
 document.addEventListener('DOMContentLoaded', function() {
+  chrome.storage.sync.get(options, (items) => {
+    for (let key in items) {
+       document.getElementById(key).checked = items.enabled;
+    }
+ });
+ // add option change event listeners to each checkbox that immediately persist option changes
+ for (const key in options) {
+    document.getElementById(key).addEventListener('change', (event) => {
+       options[key] = event.target.checked;
+       chrome.storage.sync.set({options}, () => {
+          // Update status to let user know changes were saved.
+          const status = document.getElementById('status');
+          status.textContent = 'Changes saved.';
+          setTimeout(() => {
+             status.textContent = '';
+          }, 750);
+       });
+    });
+ }
+ // if enabled state is changes in the application, automatically set/remove checkmark for 'enabled'.
+ chrome.storage.onChanged.addListener((changes, area) => {
+    if (area === 'sync' && changes.options?.newValue) {
+       changes = changes.options.newValue;
+       options.enabled = changes.enabled;
+       document.getElementById('enabled').checked = options.enabled;
+    }
+ });
   // Add an event listener to the menu items to update the selected class and show the corresponding content
   const menuItems = document.querySelectorAll('.menu a');
   const categoryContents = document.querySelectorAll('.category-content');
@@ -27,26 +54,4 @@ document.addEventListener('DOMContentLoaded', function() {
       categoryContents[index].style.display = 'block';
     });
   });
-  for (const key in options) {
-    document.getElementById(key).addEventListener('change', (event) => {
-       options[key] = event.target.checked;
-       chrome.storage.sync.set({options}, () => {
-          // Update status to let user know changes were saved.
-          const status = document.getElementById('status');
-          status.textContent = 'Changes saved.';
-          setTimeout(() => {
-             status.textContent = '';
-          }, 750);
-       });
-    });
- }
-
-   // if enabled state is changes in the application, automatically set/remove checkmark for 'enabled'.
-   chrome.storage.onChanged.addListener((changes, area) => {
-    if (area === 'sync' && changes.options?.newValue) {
-       changes = changes.options.newValue;
-       options.enabled = changes.enabled;
-       document.getElementById('enabled').checked = options.enabled;
-    }
- })
 });
